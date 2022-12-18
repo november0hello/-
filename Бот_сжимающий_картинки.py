d4059f7361975ff267ecc1a6e4c2d44a59136e26
november0hello <120821351+november0hello@users.noreply.github.com>
2022-12-18 15:16:52 +0300

    Update Бот_сжимающий_картинки.py

--- Бот_сжимающий_картинки.py
+++ Бот_сжимающий_картинки.py
@@ -9,14 +9,18 @@ from telepot.loop import MessageLoop
 import time
 import cv2
 
+if not os.path.exists("downloads"):
+    os.makedirs("downloads")
+Bot = telepot.Bot('5669967562:AAHWIKJEHx4z9znW0lROhTjmTVQ-HG2fz58')
+
 def resize_image(path):
-    img = cv2.imread(path)
+    image = cv2.imread(path)
     d = 4
-    width = int(img.shape[1] / d)
-    height = int(img.shape[0] / d)
+    width = int(image.shape[1] / d)
+    height = int(image.shape[0] / d)
     dim = (width, height)
 
-    resized = cv2.resize(img, dim, interpolation=cv2.INTER_LINEAR)
+    resized = cv2.resize(image, dim, interpolation=cv2.INTER_LINEAR)
 
     conv_kern = np.array([[1 / (16), 2 / (16), 1 / (16)],
                     [2 / (16), 4 / (16), 2 / (16)],
@@ -27,21 +31,20 @@ def resize_image(path):
     return path + ".png"
 
 def handle(message):
-    msg_type, chat_type, chat_id = telepot.glance(message)
+    message_type, chat_type, chat_id = telepot.glance(message)
 
-    if msg_type == "document":
+    if message_type == "document":
         if "image/png" in message["document"]["mime_type"] or "image/jpeg" in message["document"]["mime_type"]:
             file_id = message["document"]["file_id"]
         else:
             return
-    elif msg_type == "photo":
+    elif message_type == "photo":
         file_id = message["photo"][-1]["file_id"]
     else:
         return
 
     file_path = "downloads/" + Bot.getFile(file_id)["file_path"].split("/")[1]
     Bot.download_file(file_id, file_path)
-
     new_file_path = resize_image(file_path)
     with open(new_file_path, "rb") as f:
         Bot.sendDocument(chat_id, f)
@@ -49,10 +52,4 @@ def handle(message):
     os.remove(file_path)
     os.remove(new_file_path)
 
-if __name__ == "__main__":
-    if not os.path.exists("downloads"):
-        os.makedirs("downloads")
-
-    Bot = telepot.Bot('5669967562:AAHWIKJEHx4z9znW0lROhTjmTVQ-HG2fz58')
-
-    MessageLoop(Bot, handle).run_forever()   
+MessageLoop(Bot, handle).run_forever()   
